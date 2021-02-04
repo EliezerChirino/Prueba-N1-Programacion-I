9e5b4b2a6a1482a2c69c1cf1dc031bda41811fa4
EliezerChirino <78453157+EliezerChirino@users.noreply.github.com>
2021-02-04 06:08:14 +0400

    Add files via upload

---
+++ programa_1/programa_1/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Virginia
+ * Date: 03/02/2021
+ * Time: 07:56 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace programa_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int a = 20;
+			int b = 40;
+			int c = 60;
+			Console.WriteLine("a=20 b=40 c=60");
+			Console.WriteLine("Presione Enter para continuar");
+			Console.ReadLine();
+			//aacb
+			Console.WriteLine("Orden: aacb");
+			Console.WriteLine(a);
+			Console.WriteLine(a);
+			Console.WriteLine(a+b);
+			Console.WriteLine(4*10);
+			Console.WriteLine("Presione Enter para continuar");
+			Console.ReadLine();
+			
+			//bca
+			Console.WriteLine("Orden: bca");
+			Console.WriteLine(80/2);
+			Console.WriteLine(210+10-(80*2));
+			Console.WriteLine(20*1);
+			Console.WriteLine("Presione Enter para continuar");
+			Console.ReadLine();
+			
+			//bcbc
+			Console.WriteLine("Orden: bcbc");
+			Console.WriteLine(b);
+			Console.WriteLine(c);
+			Console.WriteLine(12%10*20);
+			Console.WriteLine(30+30-60+30*2);
+			Console.ReadLine();
+			Console.WriteLine("Presione Enter para finalizar");
+			
+			
+		}
+	}
+}
+	
